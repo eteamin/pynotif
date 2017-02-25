2d5f8dc88ee4d2153b90707e619ef0bca6c22eb3
Amin Etesamian <aminetesamian1371@gmail.com>
2017-02-26 00:22:13 +0330

    adding handler

---
+++ pynotif/__init__.py
@@ -0,0 +1,4 @@
+from pynotif.src.notifier import Notifier
+
+
+a = Notifier('localhost', 8383, 12)
--- pynotif/src/notifier.py
+++ pynotif/src/notifier.py
@@ -1,25 +1,38 @@
 import asyncio
 import websockets
 
+import asyncio_redis
 
 class Notifier:
     def __init__(self, host, port, db):
         self.host = host
         self.port = port
         self.db = db
+        self.r = yield from asyncio_redis.Connection.create(host=self.host, port=self.port, db=self.db)
+        self.connections = {}
         self.make_server()
 
     def make_server(self):
-        start_server = websockets.serve(self.hello, self.host, self.port)
+        start_server = websockets.serve(self.handler, self.host, self.port)
 
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
 
-    async def hello(self, websocket, path):
+    async def handler(self, websocket, path):
         name = await websocket.recv()
         print("< {}".format(name))
 
         greeting = "Hello {}!".format(name)
         await websocket.send(greeting)
         print("> {}".format(greeting))
+
+    async def _fetch(self, key):
+        return await self.r.get(key)
+
+    async def _register(self, websocket):
+        pass
+
+    async def _un_register(self, websocket):
+        pass
+
 
