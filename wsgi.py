b2aa28a6a25ef308e4ed5ce20f156e2f06fc08ca
Amin Etesamian <aminetesamian1371@gmail.com>
2017-03-03 02:18:37 +0330

    config

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,15 @@ import json
 from os import path
 
 import pynotif
+from pynotif.src.notifier import Notifier
 
-PATH_TO_CONFIG = path.abspath(path.join(path.dirname(pynotif.__file__), '..', 'configuration.json'))
+PATH_TO_CONFIG = path.abspath(path.join(path.dirname(pynotif.__file__), '..', 'development.json'))
 
 
 def load_config():
     with open(PATH_TO_CONFIG, 'r') as config:
         return json.load(config)
+
+
+def load_app():
+
